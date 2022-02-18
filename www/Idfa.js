ceb745108627e59788b5efb5e8051cc897ca6a3b
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2022-02-18 09:28:36 +0000

    Update Idfa.js

--- www/Idfa.js
+++ www/Idfa.js
@@ -1,21 +1,11 @@
 var exec = require("cordova/exec");
-var PLUGIN_NAME = "Idfa";
 
-module.exports = {
-    TRACKING_PERMISSION_NOT_DETERMINED: 0,
-    TRACKING_PERMISSION_RESTRICTED: 1,
-    TRACKING_PERMISSION_DENIED: 2,
-    TRACKING_PERMISSION_AUTHORIZED: 3,
+var PLUGIN_NAME = "idfa";
 
-    getInfo: function() {
-        return new Promise(function(resolve, reject) {
-            exec(resolve, reject, PLUGIN_NAME, "getInfo", []);
-        });
-    },
+exports.getInfo = function (resolve, reject) {
+  exec(resolve, reject, PLUGIN_NAME, "getInfo", []);
+};
 
-    requestPermission: function() {
-        return new Promise(function(resolve, reject) {
-            exec(resolve, reject, PLUGIN_NAME, "requestPermission", []);
-        });
-    }
+requestPermission.getInfo = function (resolve, reject) {
+  exec(resolve, reject, PLUGIN_NAME, "requestPermission", []);
 };
