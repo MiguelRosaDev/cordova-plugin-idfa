08e1637aed17804e627cb2bc9bc2abb94f02cb1e
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2022-02-22 09:01:18 +0000

    Update Idfa.js

--- www/Idfa.js
+++ www/Idfa.js
@@ -2,7 +2,7 @@ var exec = require("cordova/exec");
 
 var PLUGIN_NAME = "Idfa";
 
-exports.getInfo = function (resolve, reject) {
+exports.getInfoAndrequestPermission = function (resolve, reject) {
   exec(resolve, reject, PLUGIN_NAME, "getInfo", []);
 };
 
